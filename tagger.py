c2d0124d33e2238aaf354aba32954b83f33dd037
Louis <louis.billaud@cbien.com>
2017-11-27 12:08:51 +0100

    Spotify search done

--- tagger.py
+++ tagger.py
@@ -3,6 +3,7 @@
 from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer
 from pprint import pprint
+from renamer import bcolors
 
 from manager import SpotifyManager
 import config
@@ -17,10 +18,20 @@ class Tagger:
             self.sp.auth()
 
     def tag(self, dir):
-        for file in os.listdir(dir):
-            if os.path.isdir(dir + file):
-                self.tag(dir + file + '/')
+        for filename in os.listdir(dir):
+            if os.path.isdir(dir + filename):
+                self.tag(dir + filename + '/')
             else:
-                song = self.djv.recognize(FileRecognizer, dir + file)
-                pprint(song)
+                song = self.djv.recognize(FileRecognizer, dir + filename)
+                song = self.sp.sp.search(q=song['song_name'], type='track', limit=1)
+                try:
+                    song_artist = song['tracks']['items'][0]['artists'][0]['name']
+                    song_name = song['tracks']['items'][0]['name']
+                    pprint(song_artist + ' - ' + song_name)
+                except IndexError:
+                    print bcolors.FAIL + "unable to get tags for " + filename + bcolors.ENDC
+                    pass
         pass
+
+    def new_tags(self, song, artist, name):
+        song =
