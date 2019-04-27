cb5ef414d17aede410aaa0c26ace04d2c2d5b2d2
Louis <louis.billaud@epita.fr>
2019-04-27 20:05:00 +0200

    Unicode Error fixed on dir

--- renamer.py
+++ renamer.py
@@ -30,15 +30,13 @@ class Renamer:
             title = unicode(audiofile.tags.get('TIT2').text[0])
             remix = " (" + unicode(audiofile.tags.get('TPE4').text[0]) + " Remix)" if audiofile.tags.get('TPE4') else ""
             new_name = artist + " - " + title + remix + ".mp3"
-            new_name = dir + new_name.replace("/", " ft. ")
+            new_name = dir.decode('utf-8') + new_name.replace("/", " ft. ")
             if new_name != song.decode('utf-8'):
                 print "renaming\n" + song.decode('utf-8') + " --> " + new_name
             os.rename(song, new_name)
         except UnicodeDecodeError as e:
-            print bcolors.FAIL + "Unicode Error with " + song+ " " + str(e) + bcolors.ENDC
-        except AttributeError as e:
+            print bcolors.FAIL + "Unicode Error with " + song + " " + str(e) + bcolors.ENDC
+        except AttributeError:
             print bcolors.FAIL + "Attribute Error with " + song + " " + " Please open this song in windows first" + bcolors.ENDC
-            pass
         except OSError as e:
             print bcolors.FAIL + "OS Error with " + song + " " + str(e) + bcolors.ENDC
-        pass
